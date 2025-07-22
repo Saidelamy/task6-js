e59f5c357b4697ac60bbe3a48f67f46976326b61
said magdy <saidmagdy511@gmail.com>
2025-07-22 20:34:20 +0300

    .

--- task5/index.js
+++ task5/index.js
@@ -4,27 +4,27 @@ function popup() {
   var newWindow = open("about.html", "", "width=200px,height=200px");
   var x = 0;
   var y = 0;
+  var maxX = screen.width - 300;
+  var maxY = screen.height - 300;
+  var direction = 1;
 
   var interval = setInterval(function () {
-    x += 100;
-    y += 100;
+    x += 100 * direction;
+    y += 100 * direction;
+
+    x = Math.max(0, Math.min(x, maxX));
+    y = Math.max(0, Math.min(y, maxY));
 
     newWindow.moveTo(x, y);
-  }, 1000);
 
-  setTimeout(function () {
+    if (x === maxX || y === maxY) {
+      direction = -1;
+    } else if ((x === 0, y === 0)) {
+      direction = 1;
+    }
+  }, 500);
+
+  if (newWindow.closed) {
     clearInterval(interval);
-
-    var interval2 = setInterval(function () {
-      x -= 100;
-      y -= 100;
-
-      newWindow.moveTo(x, y);
-    }, 1000);
-
-    setTimeout(function () {
-      clearInterval(interval2);
-      newWindow.close();
-    }, 6000);
-  }, 6000);
+  }
 }
